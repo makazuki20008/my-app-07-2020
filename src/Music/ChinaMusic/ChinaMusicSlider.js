f886836e38ad9af944695c3732f86c31d0f718c3
makazuki20008 <matjoni121@gmail.com>
2020-07-02 15:38:44 +0700

    move image china music, edit gitignore, favicon

--- src/Music/ChinaMusic/ChinaMusicSlider.js
+++ src/Music/ChinaMusic/ChinaMusicSlider.js
@@ -10,7 +10,13 @@ import NguyenLam from './Images/NguyenLam1NguoiBinhThuongBenCanhAnh.jpg';
 import TyBaHanh from './Images/TyBaHanh.jpg';
 import GiangHai from './Images/GiangHaiBatDoNhi.jpg';
 import LuaChonMatDiKyUc from './Images/LuaChonMatDiKyUc.jpg';
-import QuyKhu from './Images/LuaChonMatDiKyUc.jpg';
+import QuyKhu from './Images/QuyKhuLaiHe.jpg';
+import DanTyBa from './Images/aHR0cDovL3d3dy5ueWN6aXRoZXIuY29tL3FmeS1jb250ZW50L3VwbG9hZHMvMjAxOS8wMi9iYmNlMzFjODkwNDA2NmY1OWQ3ZTdiNGQ1NmI0NDFiYS5wbmc_p_p100_p_3D.m.png';
+import DapAn from './Images/dapancuaban.jpg';
+import LuKhach from './Images/lukhachquathoigian.jpg';
+import fcca from './Images/3fcca2c21ad25c5f1bbc766a1fdc6202.jpg';
+import a281de from './Images/79a281de727d301e52c8fac1943507ea.jpg';
+import aHR from './Images/aHR0cDovL3d3dy5ueWN6aXRoZXIuY29tL3FmeS1jb250ZW50L3VwbG9hZHMvMjAxOS8wMi9iYmNlMzFjODkwNDA2NmY1OWQ3ZTdiNGQ1NmI0NDFiYS5wbmc_p_p100_p_3D.m.png';
 
 class SliderChinaMusic extends React.Component {
     render() {
@@ -22,6 +28,15 @@ class SliderChinaMusic extends React.Component {
             { url: QueBoi },
             { url: NguyenLam },
             { url: TyBaHanh },
+            { url: DanTyBa },
+            { url: GiangHai },
+            { url: LuaChonMatDiKyUc },
+            { url: QuyKhu },
+            { url: DapAn },
+            { url: LuKhach },
+            { url: fcca },
+            { url: a281de },
+            { url: aHR },
         ];
 
         return (
